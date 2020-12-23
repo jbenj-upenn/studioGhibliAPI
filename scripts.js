0e9545c23125140f9cf5df907ced6ed019ca2245
Joshua Benjamin <jbenj@sas.upenn.edu>
2020-12-23 18:49:45 -0500

    200 and 404 messages sent back to user

--- scripts.js
+++ scripts.js
@@ -3,14 +3,18 @@ var request = new XMLHttpRequest()
 
 // Open a new connection using the GET request on the URL endpoint
 request.open('GET', 'https://ghibliapi.herokuapp.com/films', true)
-request.onload = function() {
+request.onload = function () {
     //Begin accessing JSON data here
     var data = JSON.parse(this.response)
 
+    if (request.status >= 200 && request.status < 400) {
+    
     data.forEach((movie) => {
         // Log each movie title
         console.log(movie.title)
     })
+} else {
+    console.log('error')
 }
 
 // Send request
